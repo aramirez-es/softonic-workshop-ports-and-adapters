34043bd726b29306d4a9fbbe79108534370d3ca7
Alberto <alberto@aramirez.es>
2014-06-25 12:27:31 +0200

    Green

--- src/Domain/UserRegistration.php
+++ src/Domain/UserRegistration.php
@@ -5,6 +5,7 @@ namespace Domain;
 class UserRegistration
 {
 	private $users = array();
+	private $followings = array();
 
 	public function signUp($username)
 	{
@@ -20,4 +21,14 @@ class UserRegistration
 	{
 		return in_array($username, $this->users, true) ? $username : null;
 	}
-} 
+
+	public function follow( $username, $username_to_follow )
+	{
+		$this->followings[$username][] = $username_to_follow;
+	}
+
+	public function getFollowings( $username )
+	{
+		return $this->followings[$username];
+	}
+}
